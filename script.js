bd11d843b1435a25fa5a6ae11355a529824fd3f3
yaswanth reddy <122858145+yerrapareddygari@users.noreply.github.com>
2026-05-11 09:48:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const ACCESS_HASH = "b8f3c2a1d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1";
+const ACCESS_HASH = "8f434346648f6b96df89dda901c5176b10a6d83961dd3c1ac88b59b2dc327aa4";
 
 // Global Variables
 let uploadedMedia = [];
